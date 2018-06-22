ba3159db530b3d36ebc7115f3ca3df6b06cdab74
Richard Pringle <rpring9@gmail.com>
2018-06-22 11:52:18 -0400

    WIP

--- lib/riakchain/node.ex
+++ lib/riakchain/node.ex
@@ -10,10 +10,28 @@ defmodule Riakchain.Node do
     GenServer.call(__MODULE__, command)
   end
 
+  def restart() do
+    GenServer.call(__MODULE__, :restart)
+  end
+
   ### GenServer Server API
   def init(_) do
+    IO.puts("GenServer PID:")
+    IO.inspect(self())
+
     port = Port.open({:spawn, "node node.js"}, [:binary, :exit_status])
+
+    receive do
+      {^port, {:data, "started\n"}} -> IO.puts("working")
+    end
+    IO.puts("still working")
+
     {:ok, port}
+  end
+
+  def handle_call(:restart, _from, port) do
+    Port.close(port)
+    {:reply, :ok, spawn_node_server()}
   end
 
   def handle_call(command, _from, port) do
@@ -22,13 +40,26 @@ defmodule Riakchain.Node do
     {:reply, result, port}
   end
 
-  def handle_info({_port, {:exit_status, _status}}, _) do
-    port = Port.open({:spawn, "node node.js"}, [:binary, :exit_status])
+  def handle_info({_from, {:data, command }}, port) do
+    IO.puts("command:")
+    IO.inspect(command)
+
+    Port.command(port, "RUNNING")
+
     {:noreply, port}
+  end
+
+  def handle_info({_from, {:exit_status, _status}}, _state) do
+    {:noreply, spawn_node_server()}
   end
 
   def handle_info(message, port) do
     IO.inspect(message)
     {:noreply, port}
+  end
+
+  ## Privates ##
+  defp spawn_node_server() do
+    Port.open({:spawn, "node node.js"}, [:binary, :exit_status])
   end
 end
--- node.js
+++ node.js
@@ -1,8 +1,74 @@
-let count = 0;
+'use strict'
 
-process.stdin.on("data", data => {
-  count++;
-  process.stdout.write(data);
+const grpc = require('grpc')
+const fs = require('fs')
+const ws = fs.createWriteStream('./log.log')
 
-  if (count >= 5) process.exit(0);
-});
+const applicationMonitor = grpc.load('./protos/application-monitor.proto')
+
+const server = new grpc.Server()
+
+process.stdin.on('data', data => ws.write(data))
+process.stdin.on('close', () => server.tryShutdown(() => {}))
+
+server.addService(applicationMonitor.Monitor.service, {
+  start(
+    {
+      request: { name },
+    },
+    callback,
+  ) {
+    process.stdin.once('data', data => {
+      ws.write(
+        `\nstart callback called with data->${data &&
+          data.toString()}\nand name->${name}`,
+      )
+
+      callback(null, { name, state: applicationMonitor.State[data.toString()] })
+    })
+    process.stdout.write(name)
+  },
+  stop(
+    {
+      request: { name },
+    },
+    callback,
+  ) {
+    process.stdin.once('data', data => {
+      ws.write('\n\nthis is going to crash')
+
+      process.exit(1)
+
+      callback(null, { name, state: applicationMonitor.State[data.toString()] })
+    })
+    process.stdout.write(name)
+  },
+  restart(
+    {
+      request: { name },
+    },
+    callback,
+  ) {
+    process.stdin.once('data', data => {
+      callback(null, { name, state: applicationMonitor.State[data.toString()] })
+    })
+    process.stdout.write(name)
+  },
+  status(
+    {
+      request: { name },
+    },
+    callback,
+  ) {
+    process.stdin.once('data', data => {
+      callback(null, { name, state: applicationMonitor.State[data.toString()] })
+    })
+    process.stdout.write(name)
+  },
+})
+
+server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure())
+
+server.start()
+
+console.log('started')
